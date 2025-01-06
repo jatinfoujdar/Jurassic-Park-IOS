629997361fc770b8d4e953bd7bedc6ad29f3321b
Jatin Foujdar <jatin321001@gmail.com>
2025-01-06 21:56:24 +0530

    Initial Commit

---
+++ Jurassic-Park-IOS/Jurassic_Park_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jurassic_Park_IOSApp.swift
+//  Jurassic-Park-IOS
+//
+//  Created by jatin foujdar on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Jurassic_Park_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
