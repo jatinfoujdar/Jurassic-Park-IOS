ad7718fbd6123cba9b0a3f89836993d17128c1c2
Jatin Foujdar <jatin321001@gmail.com>
2025-01-08 13:08:25 +0530

    search bar functionality commit

--- Jurassic-Park-IOS/ContentView.swift
+++ Jurassic-Park-IOS/ContentView.swift
@@ -3,29 +3,50 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
-    var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                Image(predator.image) 
-                    .resizable()
-                    .scaledToFit()
-                    .shadow(color: .white, radius: 1)
-                    .frame(width: 100, height: 100)
-                
-                VStack {
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
-                }
-                .padding()
+    @State var searchText = ""
+    
+    var filterDinos: [ApexPredator]{
+        if searchText.isEmpty{
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter { predator in
+                predator.name.localizedStandardContains(searchText)
             }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack{
+            List(filterDinos) { predator in
+                NavigationLink{
+                    
+                }label: {
+                    HStack {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .shadow(color: .white, radius: 1)
+                            .frame(width: 100, height: 100)
+                        
+                        VStack {
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                        .padding()
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
         }
         .preferredColorScheme(.dark)
     }
