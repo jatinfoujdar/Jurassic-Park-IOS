2e86828a5c9c44d89d338805982a1fdae28b59dc
Jatin Foujdar <jatin321001@gmail.com>
2025-01-10 14:07:47 +0530

    annotaion commit

--- Jurassic-Park-IOS/View/PredatorMap.swift
+++ Jurassic-Park-IOS/View/PredatorMap.swift
@@ -2,12 +2,25 @@ import SwiftUI
 import MapKit
 
 struct PredatorMap: View {
+    let predators = Predators()
     @State var position : MapCameraPosition
     var body: some View {
-        Map(position: $position)
+        Map(position: $position){
+            ForEach(predators.apexPredators){predator in
+                Annotation(predator.name, coordinate: predator.location){
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .shadow(color: .white,radius: 3)
+                        .scaleEffect(x: -1)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    PredatorMap(position: .camera(MapCamera(centerCoordinate: Predators().apexPredators[10].location, distance: 1000, heading: 250, pitch: 80)))
+    PredatorMap(position: .camera(MapCamera(centerCoordinate: Predators().apexPredators[2].location, distance: 1000, heading: 250, pitch: 80)))
+        .preferredColorScheme(.dark)
 }
